eb77e69f6f16d2132a590385fc051337fd06df9c
Fatih Deniz <deniz.fatih@hotmail.de>
2024-03-07 23:09:49 +0100

    Effective Java - Item14b

---
+++ src/main/java/com/effectivejava/tutorial/effectivejava/Chapter2and3_CommonMethodsToAllObjects/item14b_Comparable/ItemFourteenBTest.java
@@ -0,0 +1,8 @@
+package com.effectivejava.tutorial.effectivejava.Chapter2and3_CommonMethodsToAllObjects.item14b_Comparable;
+
+public class ItemFourteenBTest {
+
+    public static void main(String[] args) {
+
+    }
+}
